c80d4a00f45773c4525a625384ff931c3a265a4b
morodomi <morodomi@gmail.com>
2012-09-04 20:50:59 +0900

    Sample Answer for Question 3

--- gen/net/morodomi/lecture6/R.java
+++ gen/net/morodomi/lecture6/R.java
@@ -14,9 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int result=0x7f050002;
+        public static final int image=0x7f050001;
         public static final int start=0x7f050000;
-        public static final int time=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -25,6 +24,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int end_time=0x7f040003;
         public static final int finished=0x7f040005;
+        public static final int image=0x7f040008;
         public static final int loading=0x7f040006;
         public static final int ok=0x7f040007;
         public static final int result=0x7f040004;
--- src/net/morodomi/lecture6/HttpActivity.java
+++ src/net/morodomi/lecture6/HttpActivity.java
@@ -17,22 +17,27 @@ import org.apache.http.util.EntityUtils;
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix.ScaleToFit;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 
 /**
- * Activity for Android Lecture 6
- * Use Http Connections
+ * Activity for Android Lecture 6 Use Http Connections
+ * 
  * @author Masahiro Morodomi <morodomi at gmail.com>
- *
+ * 
  */
-public class HttpActivity extends Activity implements OnClickListener{
+public class HttpActivity extends Activity implements OnClickListener {
 	private static final int DIALOG_PROGRESS = 0;
-	private static final String TWITTER_URL = "http://search.twitter.com/search.json?q=tokyo";
+	private static final String TWITTER_URL = "http://www.google.co.jp/images/srpr/logo3w.png";
 	private static final int TIMEOUT = 10000;
 
 	/** Called when the activity is first created. */
@@ -40,7 +45,7 @@ public class HttpActivity extends Activity implements OnClickListener{
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		// set onc click listener
 		findViewById(R.id.start).setOnClickListener(this);
 	}
@@ -54,7 +59,7 @@ public class HttpActivity extends Activity implements OnClickListener{
 	/** Called by Android Framework with showDialog(id) method */
 	@Override
 	protected Dialog onCreateDialog(int id) {
-		switch(id) {
+		switch (id) {
 		// if id matches DIALOG_PROGRESS, show progress dialog
 		case DIALOG_PROGRESS:
 			ProgressDialog progressDialog = new ProgressDialog(this);
@@ -67,20 +72,16 @@ public class HttpActivity extends Activity implements OnClickListener{
 
 	/**
 	 * private class that connect to web server asynchronously.
+	 * 
 	 * @author Masahiro Morodomi <morodomi at gmail.com>
 	 */
 	class AsyncHttpConnect extends AsyncTask<String, Void, HttpResponse> {
-		// save start and end time
-		private long startTime;
-		private long endTime;
-
 		@Override
-		protected void onPreExecute() {}
+		protected void onPreExecute() {
+		}
 
 		@Override
 		protected HttpResponse doInBackground(String... url) {
-			// saving start time
-			startTime = System.currentTimeMillis();
 			// prepare return object
 			HttpResponse response = null;
 			try {
@@ -88,32 +89,41 @@ public class HttpActivity extends Activity implements OnClickListener{
 				HttpConnectionParams.setConnectionTimeout(params, TIMEOUT);
 				HttpConnectionParams.setSoTimeout(params, TIMEOUT);
 				HttpClient httpClient = new DefaultHttpClient(params);
-				httpClient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
-				httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.RFC_2109);
+				httpClient.getParams().setParameter(
+						CoreProtocolPNames.PROTOCOL_VERSION,
+						HttpVersion.HTTP_1_1);
+				httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY,
+						CookiePolicy.RFC_2109);
 				HttpGet get = new HttpGet(url[0]);
-				get.setHeader("Accept", "text/html,application/xml,application/json,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5;");
-				get.setHeader("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
+				get.setHeader(
+						"Accept",
+						"text/html,application/xml,application/json,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5;");
+				get.setHeader("Content-Type",
+						"application/x-www-form-urlencoded;charset=utf-8");
 				response = httpClient.execute(get);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				Log.e("Lecture 6", e.getMessage(), e);
 			}
-			endTime = System.currentTimeMillis();
 			// passing the value to onPostExecute
 			return response;
 		}
-	
+
 		@Override
 		protected void onPostExecute(HttpResponse response) {
-			// display time difference
-			((TextView) findViewById(R.id.time)).setText("Time Diff: " + (endTime - startTime) + " ms");
-			if(response != null && response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
-				String result = null;
+			if (response != null
+					&& response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 				try {
-					result = EntityUtils.toString(response.getEntity(), "UTF-8");
+					// get byte buffer of response
+					byte[] bytes = EntityUtils
+							.toByteArray(response.getEntity());
+					// convert byte buffer to bitmap
+					Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0,
+							bytes.length);
+					// set bitmap image to ImageView
+					((ImageView) findViewById(R.id.image)).setImageBitmap(bitmap);
 				} catch (Exception e) {
 					Log.e("Lecture 6", e.getMessage(), e);
 				}
-				((TextView) findViewById(R.id.result)).setText("Result: \n" + result);
 			}
 		}
 	}
